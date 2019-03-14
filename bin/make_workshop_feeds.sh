1930400fbb19529016058e3b52bd4938d9005835
François Michonneau <francois.michonneau@gmail.com>
2019-03-14 18:37:42 +0100

    remove _plain suffix in file names

--- bin/make_workshop_feeds.sh
+++ bin/make_workshop_feeds.sh
@@ -10,25 +10,35 @@ CARPENTRIES_PROGRAMS=('swc' 'dc' 'lc' 'ttt')
 
 ## ALL workshops ---------------------------------------------------------------
 
-curl "$REDASH_API_WORKSHOPS" | jq '.query_result.data.rows' > "$OUTPUT_PATH"/all_workshops_plain.json
+curl "$REDASH_API_WORKSHOPS" | jq '.query_result.data.rows' > "$OUTPUT_PATH"/all_workshops.json
 
 ## Past workshops --------------------------------------------------------------
 
 ## all
-jq 'map(select(.end_date | strptime("%Y-%m-%d")? | mktime < now)) | sort_by(.start_date | strptime("%Y-%m-%d")? | mktime | reverse)' < "$OUTPUT_PATH"/all_workshops_plain.json > "$OUTPUT_PATH"/all_past_workshops_plain.json
+jq '
+  map(select(.end_date | strptime("%Y-%m-%d")? | mktime < now)) |
+  sort_by(.start_date | strptime("%Y-%m-%d")? | mktime | reverse)
+' < "$OUTPUT_PATH"/all_workshops.json > "$OUTPUT_PATH"/all_past_workshops.json
 
 ## for each program
 for prgm in "${CARPENTRIES_PROGRAMS[@]}"; do
-    jq "map(select(.tag_name == (\"$prgm\" | ascii_upcase)))" < "$OUTPUT_PATH"/all_past_workshops_plain.json > "$OUTPUT_PATH"/"$prgm"_past_workshops_plain.json
+    jq "
+     map(select(.tag_name == (\"$prgm\" | ascii_upcase)))
+    " < "$OUTPUT_PATH"/all_past_workshops.json > "$OUTPUT_PATH"/"$prgm"_past_workshops.json
 done
 
 
 ## Upcoming workshops ----------------------------------------------------------
 
 ## all
-jq 'map(select(.end_date | strptime("%Y-%m-%d")? | mktime >= now)) | sort_by(.start_date | strptime("%Y-%m-%d")? | mktime)' < "$OUTPUT_PATH"/all_workshops_plain.json > "$OUTPUT_PATH"/all_upcoming_workshops_plain.json
+jq '
+  map(select(.end_date | strptime("%Y-%m-%d")? | mktime >= now)) |
+  sort_by(.start_date | strptime("%Y-%m-%d")? | mktime)
+' < "$OUTPUT_PATH"/all_workshops.json > "$OUTPUT_PATH"/all_upcoming_workshops.json
 
 ## for each program
 for prgm in "${CARPENTRIES_PROGRAMS[@]}"; do
-    jq "map(select(.tag_name == (\"$prgm\" | ascii_upcase)))" < "$OUTPUT_PATH"/all_upcoming_workshops_plain.json > "$OUTPUT_PATH"/"$prgm"_upcoming_workshops_plain.json
+    jq "
+     map(select(.tag_name == (\"$prgm\" | ascii_upcase)))
+    " < "$OUTPUT_PATH"/all_upcoming_workshops.json > "$OUTPUT_PATH"/"$prgm"_upcoming_workshops.json
 done
