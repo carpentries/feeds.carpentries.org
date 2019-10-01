92612269c2bfe642adc35b79daa635c58ca4787f
maneesha sane <maneeshasane@gmail.com>
2019-10-01 10:25:33 -0400

    update GITHUB_TOKEN - new env var in travis

--- python/lesson_contributor_count.py
+++ python/lesson_contributor_count.py
@@ -2,7 +2,7 @@
 import requests
 import json
 
-token = "GITHUB_PAT"
+token = "GITHUB_TOKEN"
 
 ALL_ORG_REPOS = ["datacarpentry/ecology-workshop", "datacarpentry/spreadsheet-ecology-lesson", "datacarpentry/OpenRefine-ecology-lesson", "datacarpentry/sql-ecology-lesson", "datacarpentry/R-ecology-lesson", "datacarpentry/python-ecology-lesson", "datacarpentry/python-ecology-lesson-es", "datacarpentry/genomics-workshop", "datacarpentry/organization-genomics", "datacarpentry/shell-genomics", "datacarpentry/wrangling-genomics", "datacarpentry/cloud-genomics", "datacarpentry/genomics-r-intro", "datacarpentry/socialsci-workshop", "datacarpentry/spreadsheets-socialsci", "datacarpentry/openrefine-socialsci", "datacarpentry/r-socialsci", "datacarpentry/python-socialsci", "datacarpentry/sql-socialsci", "datacarpentry/geospatial-workshop", "datacarpentry/organization-geospatial", "datacarpentry/r-intro-geospatial", "datacarpentry/r-raster-vector-geospatial", "carpentries/instructor-training", "swcarpentry/shell-novice", "swcarpentry/git-novice", "swcarpentry/python-novice-inflammation", "swcarpentry/python-novice-gapminder", "swcarpentry/r-novice-inflammation", "swcarpentry/r-novice-gapminder", "swcarpentry/shell-novice-es", "swcarpentry/git-novice-es", "swcarpentry/r-novice-gapminder-es", "swcarpentry/make-novice", "swcarpentry/matlab-novice-inflammation", "swcarpentry/sql-novice-survey", "swcarpentry/hg-novice", "librarycarpentry/lc-data-intro", "librarycarpentry/lc-shell", "librarycarpentry/lc-open-refine", "librarycarpentry/lc-git", "librarycarpentry/lc-spreadsheets", "librarycarpentry/lc-sql", "librarycarpentry/lc-webscraping", "librarycarpentry/lc-python-intro", "librarycarpentry/lc-data-intro-archives",]
 
