412a179d0c6657222d9205dc54bd6ffbab81cd8f
François Michonneau <francois.michonneau@gmail.com>
2019-10-01 11:22:58 +0200

    add function to extract github topics as new key/value in output

--- R/curriculum_feed.R
+++ R/curriculum_feed.R
@@ -2,6 +2,18 @@ library(gh)
 library(jsonlite)
 library(purrr)
 
+LIFE_CYCLE_TAGS <- c("pre-alpha", "alpha", "beta", "stable")
+COMMON_TAGS <- c(
+  "carpentries",
+  "carpentries-incubator",
+  "carpentries-lesson",
+  "carpentryconnect",
+  "data-carpentry",
+  "datacarpentry",
+  "education",
+  "lesson"
+)
+
 `%<<%` <- function(x, y) {
   if (identical(length(x), 0L)) return(y)
   if (is.null(x) || identical(x, "") ||
@@ -62,8 +74,65 @@ get_list_repos <- function(org) {
 }
 
 make_incubator_feed <- function(path, ...) {
+##' @param data the data frame that contains the column `github_topics` from
+##'   which the tags should be extracted
+##' @param new_col_name name of the new column that will contain the extracted
+##'   tags
+##' @param dict the dictionary of tags (as a character vector) against which the
+##'   content of the `github_topics` column will be compared
+##' @param approach when set to `include` the tag(s) that match(es) the content
+##'   of the vector specified in `dict` will be extracted to create the content
+##'   of the new column; when set to `exclude` the tag(s) that match(es) the
+##'   content of the vector specified in `dict` will be excluded to create the
+##'   content of the new column. For instance if the column `github_topics` has
+##'   the values `c("tag1", "tag2")` and `dict` is `"tag1"`, the new column will
+##'   have the value `tag1` when using `include` and `tag2` when using
+##'   `exclude`.
+##' @param allow_multiple can the resulting new column contain more than one
+##'   tag?
+##' @param allow_empty can the resulting new column contain an empty value?
+extract_tag <- function(data,
+                        new_col_name,
+                        dict,
+                        approach = c("include", "exclude"),
+                        allow_multiple,
+                        allow_empty
+                        ) {
 
   get_list_repos("carpentries-incubator") %>%
+  approach <- match.arg(approach)
+
+  if (identical(approach, "include")) {
+    f1 <- intersect
+    f2 <- setdiff
+  } else if (identical(approach, "exclude")) {
+    f1 <- setdiff
+    f2 <- intersect
+  } else {
+    stop("invalid value for approach")
+  }
+
+  data %>%
+      dplyr::mutate(!!rlang::quo_name(rlang::enquo(new_col_name)) := purrr::pmap(.,
+        function(github_topics, full_name, ...) {
+          extracted_tag <- f1(github_topics, dict)
+          if ((!allow_multiple) && length(extracted_tag) > 1) {
+            stop("More than one tag detected for: ", full_name)
+          }
+          if (length(extracted_tag) == 0) {
+            if (! allow_empty) {
+              stop("No tag found among (", paste(dict, collapse = ", "), ") ",
+                "for repo: ", full_name)
+            }
+            return("")
+          }
+          extracted_tag
+        })) %>%
+      dplyr::mutate(github_topics = purrr::pmap(., function(github_topics, ...) {
+        f2(github_topics, dict)
+      }))
+
+}
     dplyr::select(-private) %>%
     dplyr::filter(grepl("lesson", github_topics)) %>%
     jsonlite::write_json(path = path)
