33daab1cf77e7a915bc9a3f5e9da901afc16da36
maneesha sane <maneeshasane@gmail.com>
2019-10-02 16:21:02 -0400

    better formed json output file

--- python/lesson_contributor_count.py
+++ python/lesson_contributor_count.py
@@ -7,11 +7,9 @@ token = os.environ['GITHUB_TOKEN']
 
 ALL_ORG_REPOS = ["datacarpentry/ecology-workshop", "datacarpentry/spreadsheet-ecology-lesson", "datacarpentry/OpenRefine-ecology-lesson", "datacarpentry/sql-ecology-lesson", "datacarpentry/R-ecology-lesson", "datacarpentry/python-ecology-lesson", "datacarpentry/python-ecology-lesson-es", "datacarpentry/genomics-workshop", "datacarpentry/organization-genomics", "datacarpentry/shell-genomics", "datacarpentry/wrangling-genomics", "datacarpentry/cloud-genomics", "datacarpentry/genomics-r-intro", "datacarpentry/socialsci-workshop", "datacarpentry/spreadsheets-socialsci", "datacarpentry/openrefine-socialsci", "datacarpentry/r-socialsci", "datacarpentry/python-socialsci", "datacarpentry/sql-socialsci", "datacarpentry/geospatial-workshop", "datacarpentry/organization-geospatial", "datacarpentry/r-intro-geospatial", "datacarpentry/r-raster-vector-geospatial", "carpentries/instructor-training", "swcarpentry/shell-novice", "swcarpentry/git-novice", "swcarpentry/python-novice-inflammation", "swcarpentry/python-novice-gapminder", "swcarpentry/r-novice-inflammation", "swcarpentry/r-novice-gapminder", "swcarpentry/shell-novice-es", "swcarpentry/git-novice-es", "swcarpentry/r-novice-gapminder-es", "swcarpentry/make-novice", "swcarpentry/matlab-novice-inflammation", "swcarpentry/sql-novice-survey", "swcarpentry/hg-novice", "librarycarpentry/lc-data-intro", "librarycarpentry/lc-shell", "librarycarpentry/lc-open-refine", "librarycarpentry/lc-git", "librarycarpentry/lc-spreadsheets", "librarycarpentry/lc-sql", "librarycarpentry/lc-webscraping", "librarycarpentry/lc-python-intro", "librarycarpentry/lc-data-intro-archives",]
 
-contrib_dict = {}
+contributor_count = []
 
 for repo in ALL_ORG_REPOS:
-    
-    contrib_dict[repo] = 0
     
     contrib_count = 0
     
@@ -21,14 +19,11 @@ for repo in ALL_ORG_REPOS:
         r = requests.get(url=CONTRIBS)
         contrib_count += len(r.json())
         if len(r.json()) == 0:
-            break      
-        
-        
-    contrib_dict[repo] = contrib_count
-    
-    print(repo, contrib_count)
+            break
 
-print(contrib_dict)
+    contributor_count.append({'lesson':repo, 'contributor_count':contrib_count})
 
 with open('./lesson_contributor_count.json', 'w') as fp:
-    json.dump(contrib_dict, fp)
+    json.dump(contributor_count, fp)
+
+
