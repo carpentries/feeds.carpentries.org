832d27f56ec2c79c16057e3e0d5dba70f869cb5c
maneesha sane <ahseenam@gmail.com>
2020-08-03 02:58:47 -0400

    remove creation of html page (#42)
    Co-authored-by: maneesha sane <maneeshasane@gmail.com>

--- python/instructor_training_completion_rates.py
+++ python/instructor_training_completion_rates.py
@@ -75,14 +75,3 @@ plt.savefig('plot_checkout_completion_rates.svg')
 
 # Save dataframe to json
 completion_rates.to_json('checkout_completion_rates.json')
-
-crt = completion_rates.to_html()
-
-with open('checkout_completion_rates.html', 'w') as f:
-    f.write("Of all trainees who completed our two day Instructor Training course, the number of people ")
-    f.write("who completed 0, 1, or 2 checkout steps, and who completed all three steps (and were badged as Instructors.)")
-    f.write("<br>")
-    f.write("<a href='checkout_completion_rates.json'>Raw JSON data</a>")
-    f.write("<img src='plot_checkout_completion_rates.svg' />")
-
-    f.write(crt)
