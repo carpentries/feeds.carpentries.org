3b087f00931c80209a4c488f20a0a3952e308e24
Maneesha Sane <ahseenam@gmail.com>
2022-10-19 09:57:31 -0400

    add script for maintainers

--- bin/make_active_roles.sh
+++ bin/make_active_roles.sh
@@ -20,3 +20,21 @@ REDASH_API_TRAINERS="https://redash.carpentries.org/api/queries/523/results.json
 curl "$REDASH_API_TRAINERS" | jq '
      .query_result.data.rows 
 ' > "$OUTPUT_PATH"/all_trainers.json
+
+
+
+## Check that the API key is available
+if [ -z "$REDASH_KEY_QUERY524" ]
+then
+    echo "The variable REDASH_KEY_QUERY524 is not set"
+    exit 1
+fi
+
+REDASH_API_MAINTAINERS="https://redash.carpentries.org/api/queries/524/results.json?api_key=$REDASH_KEY_QUERY524"
+
+
+## ALL Trainers  ---------------------------------------------------------------
+
+curl "$REDASH_API_MAINTAINERS" | jq '
+     .query_result.data.rows 
+' > "$OUTPUT_PATH"/all_maintainers.json
