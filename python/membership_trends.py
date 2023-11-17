a7bb128e9b2d7ef8eafbb6ee9603c514b9312228
Maneesha Sane <ahseenam@gmail.com>
2023-11-17 14:11:15 -0500

    save membership trends json file in _data folder

--- python/membership_trends.py
+++ python/membership_trends.py
@@ -75,6 +75,6 @@ ax.legend(['Member count', '6 Month Moving Average'], fontsize = 16)
 # Save plot image and json
 plt.savefig('./plot_membership_trends.svg')
 member_count_by_month_df['start_month'] = member_count_by_month_df['start_month'].dt.strftime('%Y-%m-%d')
-member_count_by_month_df.to_json('membership_trends.json')
+member_count_by_month_df.to_json('_data/membership_trends.json')
 
 
