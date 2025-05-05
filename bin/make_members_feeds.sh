9b9d1c12b598cf2a233bf6a7402019508b30e87e
maneesha <829690+maneesha@users.noreply.github.com>
2025-05-05 16:03:03 -0400

    typo

--- bin/make_members_feeds.sh
+++ bin/make_members_feeds.sh
@@ -31,7 +31,7 @@ curl "$REDASH_API_INSTRUCTORS" |
    .is_mentor = contains({badges: "8"}) |
    .is_mentee = contains({badges: "9"}) |
    .is_instructor = contains({badges: "13"}) | 
-   .is_lesson_developer = containes({badges: "14"})
+   .is_lesson_developer = contains({badges: "14"})
    )' > /tmp/badged_people_raw.json
 
 ## Make sure the file was successfully downloaded
