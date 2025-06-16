4ba99bd93e0f43710646b17d4f8cd8ed8e4ceb64
maneesha <829690+maneesha@users.noreply.github.com>
2025-06-16 14:30:04 -0400

    omit unpublished newsletter items

--- bin/make_newsletter_feed.sh
+++ bin/make_newsletter_feed.sh
@@ -18,4 +18,4 @@ then
     exit 1
 fi
 
-curl --request GET --url "https://us14.api.mailchimp.com/3.0/campaigns?count=1000&list_id=50c3e6d6fe&fields=campaigns.archive_url,campaigns.settings.subject_line,campaigns.send_time&sort_field=send_time&sort_dir=DESC"  --user anystring:$MAILCHIMP_NEWSLETTER_KEY | jq '[.campaigns[] | {url: .archive_url, pub_date: .send_time, subject: .settings.subject_line}  ]' > "$OUTPUT_PATH"/newsletter.json
+curl --request GET --url "https://us14.api.mailchimp.com/3.0/campaigns?count=1000&list_id=50c3e6d6fe&fields=campaigns.archive_url,campaigns.settings.subject_line,campaigns.send_time&sort_field=send_time&sort_dir=DESC"  --user anystring:$MAILCHIMP_NEWSLETTER_KEY | jq '[.campaigns[] | {url: .archive_url, pub_date: .send_time, subject: .settings.subject_line} | select(.pub_date != "")   ]' >  "$OUTPUT_PATH"/newsletter.json
