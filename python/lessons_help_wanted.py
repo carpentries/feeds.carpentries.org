15278601d7682e68f997ff5a2fb2bfdb47f06aa9
Maneesha Sane <ahseenam@gmail.com>
2025-10-23 14:34:41 -0400

    re-write help-wanted script to avoid GH api search limits

--- python/lessons_help_wanted.py
+++ python/lessons_help_wanted.py
@@ -1,30 +1,23 @@
+#!/usr/bin/env python
+
 # Import all the things
 import requests
 import os
-from datetime import datetime
-import time
 import json
 
-# Set global vars
-GH_ORG_DC = "datacarpentry"
-GH_ORG_SWC = "swcarpentry"
-GH_ORG_LC = "librarycarpentry"
-GH_ORG_LAB = "carpentries-lab"
-GH_ORG_INC = "carpentries-incubator"
-ALL_ORGS = [GH_ORG_DC, GH_ORG_LC, GH_ORG_SWC, GH_ORG_LAB, GH_ORG_INC,]
-GH_SEARCH_BASE_URL = "https://api.github.com/search"
-GH_ORG_BASE_URL = "https://api.github.com/orgs"
-
-topics = "stable,helpwanted-list"
-labels = "\"help wanted\",\"good first issue\""
-
-# Authentication
+# Authenticate
 GH_TOKEN = os.environ['GITHUB_PAT']
 
 headers = {
     "Authorization": f"token {GH_TOKEN}",
     "Accept": "application/vnd.github+json"
 }
+
+
+# Identify all Carpentries organizations
+ORGS = ["swcarpentry", "datacarpentry", "librarycarpentry", "carpentries"]
+ORG_TOPICS = ['stable', 'helpwanted-list']
+ISSUE_LABELS = ["good first issue", "help wanted"]
 
 
 def get_json(url, headers, params=None):
@@ -42,163 +35,169 @@ def get_json(url, headers, params=None):
     return r.json()
 
 
-def get_repos_by_topic(org, repo_topics):
+def get_org_info(org):
     """
-    Function takes:
-    * GH organization name as a string (eg "swcarpentry" or "carpentries")
-    * GH repo topics to search for as a csv string.
-    Queries for all GH repos in that organization that have the topic "stable".
-    Returns list of dicts with repo and org data (to be used to gather help-wanted issues in that repo).
+    Takes org name as a string and returns
+    a dict with the org name and url
     """
 
-    print(f"Searching for repos in {org} with topics: {topics}")
-    # Get GH organization name and url
-    org_url = f"{GH_ORG_BASE_URL}/{org}"
+    url = f"https://api.github.com/orgs/{org}"
+    org_info = get_json(url, headers)
+    # r = requests.get(url, headers=headers)
+    # data = org_info.json()
+    # print(data['name'], data['description'], data['html_url'])
+    org_dict = {'org_url': org_info['html_url'], 'org_full_name': org_info['name']}
+    return org_dict
 
-    gh_org = get_json(org_url, headers)
 
-    if 'name' in gh_org:
-        org_full_name = gh_org['name']
+def get_repos_as_dict(org, topics):
+    """
+    Get all repositories in a GitHub org that have any of the specified topics.
+    Takes two parameters:
+    org: GH organization name
+    topics: list of topics as strings
+    Returns dict with repo name, description, and url
+    """
+
+    print(f"\n**Retrieving all repos from {org} organization with topics: {topics}**\n")
+    repos = []
+    page = 1
+
+    while True:
+        url = f"https://api.github.com/orgs/{org}/repos?per_page=100&page={page}"
+        data = get_json(url, headers)
+
+        if not data:
+            break
+
+        for repo in data:
+            if repo.get("archived"):
+                # Skip archived repos
+                continue
+
+            repo_name = repo["name"]
+
+            topics_url = f"https://api.github.com/repos/{org}/{repo_name}/topics"
+            topics_response = get_json(topics_url, headers)
+
+            repo_topics = topics_response.get("names", [])
+
+            repo_dict = {}
+            if any(topic in repo_topics for topic in topics):
+                repo_url = repo['html_url']
+                repo_description = repo['description']
+                print(f" - {repo_name}, {repo_url}")
+                repo_dict['repo_name'] = repo_name
+                repo_dict['repo_description'] = repo_description
+                repo_dict['repo_url'] = repo_url
+
+                repos.append(repo_dict)
+
+        page += 1
+
+    print(f"    {len(repos)} repos returned\n")
+
+    return repos
+
+
+def get_issues_from_repo_dict(org, repo_dict, labels):
+    """
+    Function takes three params:
+    org name (str)
+    repo dict (as created in previous function)
+    labels(list)
+    Returns dict of issues with labels from that repo/org, including repo/org info
+    """
+
+    all_issues = []
+    issue_keys = ['html_url', 'title', 'created_at', 'updated_at', 'labels']
+
+    # Get org info; this will be used later when building the dict
+    org_info_dict = get_org_info(org)
+
+    repo_name = repo_dict['repo_name']
+    print(f"Fetching issues from {org}/{repo_name}...")
+    for label in labels:
+        params = {'labels': label, 'state': 'open'}
+        issues_url = f"https://api.github.com/repos/{org}/{repo_name}/issues"
+        issues = get_json(issues_url, headers, params)
+
+        if not issues:
+            continue
+
+        # Add the fetched issues to our dictionary.
+        for issue in issues:
+            issue_dict = {key: issue[key] for key in issue_keys}
+            labels_by_name = [x["name"] for x in issue_dict['labels']]
+            issue_dict['labels'] = labels_by_name
+            issue_dict['issue_title'] = issue_dict.pop('title')
+            issue_dict['issue_url'] = issue_dict.pop('html_url')
+
+            issue_dict['org'] = org
+            issue_dict['org_url'] = org_info_dict['org_url']
+            issue_dict['org_full_name'] = org_info_dict['org_full_name']
+
+            issue_dict['repo_url'] = repo_dict['repo_url']
+            issue_dict['repo_description'] = repo_dict['repo_description']
+            all_issues.append(issue_dict)
+
+    print(f" - {len(all_issues)} issues returned")
+    return all_issues
+
+
+def convert_data_types(issue_dict):
+    """
+    Takes dict of issues that includes at least the following keys:
+    ['created_at',
+     'updated_at',
+     'labels',]
+    Converts the 'labels' list and the 'created_at' and 'updated_at' dates to human readable strings
+    """
+
+    if isinstance(issue_dict, dict):
+        try:
+            issue_dict['created_at'] = issue_dict['created_at'][:10]
+            issue_dict['updated_at'] = issue_dict['updated_at'][:10]
+
+            list_string = ", ".join(issue_dict['labels'])
+            issue_dict['labels'] = list_string
+            return issue_dict
+
+        except KeyError as e:
+            print(f"Key not found: {e}")
+            return
+
     else:
-        print("Key 'name' not found.")
-        return gh_org
-
-    if 'html_url' in gh_org:
-        org_url = gh_org['html_url']
-    else:
-        print("Key 'html_url' not found.")
-        return gh_org
-
-    # Get all repos in that organization the topic "stable"
-    # Additional topics can be added separated by commas and use OR logic.
-    params = {
-        "q": f"org:{org} topic:{repo_topics}",
-    }
-
-    repos_with_topic_req = get_json(f"{GH_SEARCH_BASE_URL}/repositories", headers, params)
-
-    # repos_with_topic = repos_with_topic_req.json()['items']
-    if 'items' in repos_with_topic_req:
-        repos_with_topic = repos_with_topic_req['items']
-    else:
-        print("Key 'items' not found.")
-        return repos_with_topic_req
-
-    # Build list of dicts
-    # This will be used as base dict for each repo
-    repos_with_topic_clean = [{"repo_url": repo["html_url"],
-                               "repo_description": repo["description"],
-                               "repo_name": repo["name"],
-                               "org_name": org,
-                               "org_full_name": org_full_name,
-                               "org_url": org_url} for repo in repos_with_topic]
-    count_repos = len(repos_with_topic_clean)
-    print(f"Found {count_repos} repos.")
-    return repos_with_topic_clean
-
-
-def get_help_wanted_issues(repos_with_topic, issue_labels):
-    """
-    Function takes:
-    * list of dicts created by get_repos_by_topic()
-    * GH issue labels to search for as a csv string. Issue names with whitespace
-    must be surrounded by escaped double quotes.
-
-    Iterates over each dict to search for all issues in that repo with those labels.
-    Returns list of issues with those tags identified by repo & organization.
-    """
-
-    # Rough hacky check on if `repos_with_topic` is non-zero length and properly formatted list of dicts
-    if len(repos_with_topic) == 0:
-        print("Empty data for 'repos_with_topic'")
+        print(f"Not a dict: {issue_dict}")
         return
-    if not isinstance(repos_with_topic, list) or "org_url" not in repos_with_topic[0].keys():
-        print("Invalid source data for repos_with_topic")
-        return
-
-    print(f"Searching for issues with labels: {issue_labels}")
-    # Initialize empty list to store list of dicts for each issue
-    all_help_wanted_issues = []
-
-    # Go through each repo
-    # Search for issues labeled "help wanted" or "good first issue"
-
-    for r in repos_with_topic:
-
-        # Set up API request
-        org_name = r['org_name']
-        repo_name = r['repo_name']
-        url = f"{GH_SEARCH_BASE_URL}/issues"
-        query = f"repo:{org_name}/{repo_name} label:{issue_labels} state:open is:issue"
-
-        params = {
-                "q": query
-                }
-
-        help_wanted_issues_req = get_json(url, headers, params)
-
-        if 'items' in help_wanted_issues_req:
-            help_wanted_issues = help_wanted_issues_req['items']
-        else:
-            print("Key 'items' not found.")
-            return help_wanted_issues_req
-
-        for i in help_wanted_issues:
-
-            # Create copy of the repo dict
-            hw_issue = r.copy()
-
-            # Create csv string of label names
-            labels = [x['name'] for x in i['labels']]
-            labels = ", ".join(labels)
-
-            # Convert dates to human readable format
-            created_at = datetime.strptime(i['created_at'], "%Y-%m-%dT%H:%M:%SZ").strftime("%d %B %Y")
-            updated_at = datetime.strptime(i['updated_at'], "%Y-%m-%dT%H:%M:%SZ").strftime("%d %B %Y")
-
-            # Add fields for this issue to the dict
-            hw_issue['issue_url'] = i['html_url']
-            hw_issue['issue_title'] = i['title']
-            hw_issue['created_at'] = created_at
-            hw_issue['updated_at'] = updated_at
-            hw_issue['labels'] = labels
-
-            # Add this dict to list of all help wanted issues
-            all_help_wanted_issues.append(hw_issue)
-
-    count_issues = len(all_help_wanted_issues)
-    print(f"Found {count_issues} issues.\n")
-    return all_help_wanted_issues
 
 
-# Initialize empty list to hold all issues
-all_issues = []
+full_issue_list = []
 
-# Loop through all orgs, get repos and issues
-# Add issues to all_issues list
+for org in ORGS:
+    org_repos = get_repos_as_dict(org, ORG_TOPICS)
+    for repo in org_repos:
+        org_issues = get_issues_from_repo_dict(org, repo, ISSUE_LABELS)
+        full_issue_list.extend(org_issues)
 
-# Note rate limit is 30 requests per minute for search
-# https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28
-# Script will need modification if any LPs exceed 30 repos to query.
-# DC is currently at 22.
+# Format date and list data types
+formatted_full_issue_list = []
+for issue in full_issue_list:
+    formatted_issue = convert_data_types(issue)
+    formatted_full_issue_list.append(formatted_issue)
 
-for i in ALL_ORGS:
-    try:
-        repos = get_repos_by_topic(i, topics)
-        issues = get_help_wanted_issues(repos, labels)
-        all_issues.extend(issues)
-    except Exception as e:
-        print(f"Error with repo: '{i}', '{e}'")
-        continue
-    print("Pausing 60 seconds to reset GH API rate limit...\n")
-    time.sleep(60)
+# Sort issues by created date (most recent first)
+formatted_full_issue_list.sort(key=lambda x: x['created_at'], reverse=True)
+
+print(len(formatted_full_issue_list), 'issues retrieved')
+
+# Save output to json file 
 
 filename = '_data/lessons_help_wanted.json'
-print(f'Total {len(all_issues)} issues retrieved')
 print(f'Saving to file {filename}')
 
 try:
     with open(filename, 'w') as file:
-        json.dump(all_issues, file)
+        json.dump(formatted_full_issue_list, file)
 except Exception as e:
     print(f'An error occured when writing to {filename}: {e}')
