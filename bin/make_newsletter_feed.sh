afe8aaa41a0909a6fd9ab2fea1e44c380a4d11e3
maneesha <829690+maneesha@users.noreply.github.com>
2026-01-05 15:04:38 -0500

    fix mailchimp sorting issue
    mailchimp's API is not sorting correctly so I am trying to sort via jq

--- bin/make_newsletter_feed.sh
+++ bin/make_newsletter_feed.sh
@@ -18,4 +18,10 @@ then
     exit 1
 fi
 
-curl --request GET --url "https://us14.api.mailchimp.com/3.0/campaigns?count=1000&list_id=50c3e6d6fe&fields=campaigns.archive_url,campaigns.settings.subject_line,campaigns.send_time&sort_field=send_time&sort_dir=DESC"  --user anystring:$MAILCHIMP_NEWSLETTER_KEY | jq '[.campaigns[] | {url: .archive_url, pub_date: .send_time, subject: .settings.subject_line} | select(.pub_date != "")   ]' >  "$OUTPUT_PATH"/newsletter.json
+curl --request GET --url "https://us14.api.mailchimp.com/3.0/campaigns?count=1000&list_id=50c3e6d6fe&fields=campaigns.archive_url,campaigns.settings.subject_line,campaigns.send_time&sort_field=send_time&sort_dir=DESC"  --user anystring:$MAILCHIMP_NEWSLETTER_KEY | jq '[.campaigns[] | {url: .archive_url, pub_date: .send_time, subject: .settings.subject_line} | select(.pub_date != "")   ] | sort_by(.pub_date) | reverse' >  "$OUTPUT_PATH"/newsletter.json
+
+
+
+
+
+
